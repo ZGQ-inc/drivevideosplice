3abafeea438d4c5408d4748df91c1db09d6b4fd9
ZGQ Inc. <zgqinc@gmail.com>
2025-01-21 14:06:21 +0800

    Update gen.sh

--- gen_for_test/gen.sh
+++ gen_for_test/gen.sh
@@ -3,9 +3,10 @@
 while read -r filepath; do
     filename=$(basename "$filepath")
     
-    output="/mnt/l/Download/${filename}_output.mp4"
+    output="${filename}.MP4"
     
     ffmpeg -f lavfi -i color=c=black:s=240x240:d=5 \
            -vf "drawtext=fontfile=/path/to/font.ttf:text='${filename}':fontcolor=white:fontsize=24:x=(w-text_w)/2:y=(h-text_h)/2" \
            -c:v libx264 -pix_fmt yuv420p -y "$output"
 done < filelist.txt
+ < filelist.txt
